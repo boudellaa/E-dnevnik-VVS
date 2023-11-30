43de8b019f9237212d8d97b652ecc1a1535f62a6
kenankd <kdizdarevi1@etf.unsa.ba>
2023-11-30 17:28:21 +0100

    resolved issues

---
+++ TestProject/NastavnikTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject
+{
+    [TestClass]
+    public class NastavnikTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
