10cb53e71d0058c50a06c14def33e7728aef4ba2
nedimkrupalija <nkrupalija1@etf.unsa.ba>
2023-12-14 23:04:47 +0100

    update documentation for task 2

--- Testovi/hashTDD.cs
+++ Testovi/hashTDD.cs
@@ -8,35 +8,35 @@ using System.Threading.Tasks;
 
 namespace Testovi
 {
-    [TestClass]
-    public class hashTDD
-    {
-        private E_Dnevnik ednevnik;
+	[TestClass]
+	public class hashTDD
+	{
+		private E_Dnevnik ednevnik;
 
-        [TestInitialize]
-        public void TestInitialize()
-        {
-            ednevnik = new E_Dnevnik();
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			ednevnik = new E_Dnevnik();
 
-        }
-        [TestMethod]
-        public void HashPassword_ReturnEmptyString()
-        {
-            var salt = new byte[64];
-            Assert.AreEqual("",ednevnik.HashPassword("", out salt));
-        }
+		}
+		[TestMethod]
+		public void HashPassword_ReturnEmptyString()
+		{
+			var salt = new byte[64];
+			Assert.AreEqual("", ednevnik.HashPassword(""));
+		}
 
 
-        [TestMethod]
-        public void HashPassword_ReturnCorrectHash()
-        {
-            var password = "test123";
-            var salt = new byte[64];
-            var hashedPassword = ednevnik.HashPassword(password, out salt);
-            var result = ednevnik.VerifyPassword(password, hashedPassword, salt);
-            Assert.IsTrue(result);
-        }
-        
+		[TestMethod]
+		public void HashPassword_ReturnCorrectHash()
+		{
+			var password = "test123";
+			var salt = new byte[64];
+			var hashedPassword = ednevnik.HashPassword(password);
+			var result = ednevnik.VerifyPassword(password, hashedPassword);
+			Assert.IsTrue(result);
+		}
 
-    }
+
+	}
 }
